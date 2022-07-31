88908864abe15cb1cc8bbb159feda5b341661f76
Aaron Tan <info@aarontan.co.nz>
2022-07-31 12:23:53 +1200

    add disconnection function and add new user

--- public/script.js
+++ public/script.js
@@ -7,16 +7,51 @@ const myPeer = new Peer(undefined, {
 
 const myVideo = document.createElement('video')
 myVideo.muted = true
+const peers = {}
 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
-
   .then((stream) => {
     addVideoStream(myVideo, stream)
+
+    myPeer.on('call', (call) => {
+      call.answer(stream)
+      const video = document.createElement('video')
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+
+    socket.on('user-connected: ', (userId) => {
+      connectToNewUser(userId, stream)
+    })
   })
+
+socket.on('user-disconnected', (userId) => {
+  if (peers[userId]) peers[userId].close()
+})
+
+myPeer.on('open', (id) => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  {
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', (userVideoStream) => {
+      addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+      video.remove()
+    })
+  }
+  peers[userId] = call
+}
+
 //this is to display screen share
 // navigator.mediaDevices
 //   .getDisplayMedia({
@@ -27,13 +62,10 @@ navigator.mediaDevices
 //     addVideoStream(myVideo, stream)
 //   })
 
-myPeer.on('open', (id) => {
-  socket.emit('join-room', ROOM_ID, id)
-})
-
-socket.on('user-connected', (userId) => {
-  console.log('user connected' + userId)
-})
+//test code
+// socket.on('user-connected', (userId) => {
+//   console.log('user connected' + userId)
+// })
 
 function addVideoStream(video, stream) {
   video.srcObject = stream

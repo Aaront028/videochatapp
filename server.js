eaf9ba5dbab888f5ebbeaa984a7286f39f7737b8
Aaron Tan <info@aarontan.co.nz>
2022-07-31 15:25:46 +1200

    change port 3000 to process.env

--- server.js
+++ server.js
@@ -41,5 +41,5 @@ io.on('connection', (socket) => {
 
 // ===============================================
 
-server.listen(3000)
-// server.listen(process.env.PORT || 80)
+// server.listen(3000)
+server.listen(process.env.PORT || 80)

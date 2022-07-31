1ffaebdcf831579998378bfaffae3cce1c0058d9
Aaron Tan <info@aarontan.co.nz>
2022-07-31 13:28:29 +1200

    revert to no broadcast emit

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.get('/:room', (req, res) => {
 io.on('connection', (socket) => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId)
+    socket.to(roomId).emit('user-connected', userId)
 
     socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+      socket.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
 
+// server.listen(3000)
 server.listen(process.env.PORT || 80)
